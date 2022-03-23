fixed IMLearn\learners\MultivariateGaussian.fit - pdfs + optimize log-likelihood, fixing multivariate tests + refactoring and aesthetics.

IMLearn/learners/gaussian_estimators.py
@@ -175,7 +175,7 @@ class MultivariateGaussian:
         Then sets `self.fitted_` attribute to `True`
         """
         self.mu_ = np.mean(X, axis=0)
-        self.cov_ = np.cov(X.T, ddof=1)
+        self.cov_ = np.cov(X, ddof=1, rowvar=False)
 
         self.fitted_ = True
         return self
@@ -200,14 +200,27 @@ class MultivariateGaussian:
         """
         if not self.fitted_:
             raise ValueError(
-                "Estimator must first be fitted before calling `pdf` function")
+                "Estimator must first be fitted before calling `pdf` function"
+            )
 
         pdfs = []
-        mn = multivariate_normal(self.mu_, self.cov_)
-        for x in X:
-            pdfs.append(mn.pdf(x))
 
-        return np.ndarray(pdfs)
+        n_features = X.shape[1]
+        cov_det = np.linalg.det(self.cov_)
+        cov_inv = np.linalg.inv(self.cov_)
+
+        denominator = (((2 * np.pi) ** n_features) * cov_det) ** 0.5
+
+        for x in X:
+            diff = x - self.mu_
+            diff_T = np.transpose(diff)
+            exp_arg = -0.5 * diff_T @ cov_inv @ diff
+            numerator = np.exp(exp_arg)
+
+            x_pdf = numerator / denominator
+            pdfs.append(x_pdf)
+
+        return np.asarray(pdfs)
 
     @staticmethod
     def log_likelihood(mu: np.ndarray, cov: np.ndarray,
@@ -229,18 +242,16 @@ class MultivariateGaussian:
         log_likelihood: float
             log-likelihood calculated over all input data and under given parameters of Gaussian
         """
+        n_samples = X.shape[0]
+        n_features = X.shape[1]
+
         cov_inv = np.linalg.inv(cov)
         cov_det = np.linalg.det(cov)
 
-        s1 = 0
-        for sample in X:
-            diff = (sample - mu)
-            s1 += -0.5 * diff.T.dot(cov_inv).dot(diff)
+        log_operand = 1 / (2 * np.pi * (cov_det ** 1 / n_features))
+        first_operand = 0.5 * n_samples * n_features * np.log(log_operand)
 
-        m = X.shape[0]
-        d = X.shape[1]
+        diff = (X - mu)
+        second_operand = -0.5 * np.sum(diff @ cov_inv * diff)
 
-        log_param = 1 / (2 * np.pi * (cov_det ** (1 / d)))
-        s2 = d * m * np.log(log_param) / 2
-
-        return s1 + s2
+        return first_operand + second_operand

exercises/fit_gaussian_estimators.py
@@ -1,16 +1,18 @@
-import numpy.random
+import numpy as np
+import scipy.stats
 
 from IMLearn.learners import UnivariateGaussian, MultivariateGaussian
-
 from utils import *
 
+pio.templates.default = "simple_white"
+
+PLOT_HEIGHT = 500
 TITLE_FONT = {"family": "Times New Roman", "size": 20}
 
 TITLE_FIGURE_Q1 = "Absolute Distance Between Estimated and True Value of " \
                   "the Expectation as a Function of the Sample Size"
 TITLE_FIGURE_Q3 = "The Empirical PDF Function Under the Fitted Model"
-
-pio.templates.default = "simple_white"
+TITLE_FIGURE_Q5 = "Log-likelihood value as a Function of f1, f3"
 
 
 def test_univariate_gaussian():
@@ -22,96 +24,117 @@ def test_univariate_gaussian():
     samples = np.random.normal(real_mu, real_var, size=sample_size)
 
     univariate_gaussian = UnivariateGaussian().fit(samples)
-    print(f"({univariate_gaussian.mu_}, {univariate_gaussian.var_})")
+    print((univariate_gaussian.mu_, univariate_gaussian.var_))
 
     # Question 2 - Empirically showing sample mean is consistent
-    num_of_samples = np.linspace(10, sample_size, 100).astype(np.int64)
+    min_num_of_samples = 10
+    num_of_samples_axis = np.linspace(
+        min_num_of_samples, sample_size, 100
+    ).astype(np.int64)
 
     estimated_mean = []
-    for n in num_of_samples:
+    for num_of_samples in num_of_samples_axis:
         estimated_mean.append(
-            abs(real_mu - UnivariateGaussian().fit(samples[:n]).mu_)
+            abs(real_mu -
+                UnivariateGaussian().fit(samples[:num_of_samples]).mu_)
         )
 
     go.Figure(
-        [go.Scatter(x=num_of_samples, y=estimated_mean, mode='markers+lines')],
+        [go.Scatter(x=num_of_samples_axis, y=estimated_mean,
+                    mode='markers+lines')],
         layout=go.Layout(
             title={"text": TITLE_FIGURE_Q1, "font": TITLE_FONT},
             xaxis={"title": "Sample Size"},
             yaxis={"title": "r$|\mu-\hat\mu|$"},
-            height=500
+            height=PLOT_HEIGHT
         )).show()
 
     # Question 3 - Plotting Empirical PDF of fitted model
-
-    PDF = univariate_gaussian.pdf(samples)
+    PDFs_vector = univariate_gaussian.pdf(samples)
 
     go.Figure(
-        [go.Scatter(x=samples, y=PDF, mode='markers')],
+        [go.Scatter(x=samples, y=PDFs_vector, mode='markers')],
         layout=go.Layout(
             title={"text": TITLE_FIGURE_Q3, "font": TITLE_FONT},
             xaxis={"title": "Sample Values"},
             yaxis={"title": "PDF Values"},
-            height=500
+            height=PLOT_HEIGHT
         )).show()
 
 
 def test_multivariate_gaussian():
     # Question 4 - Draw samples and print fitted model
     real_mu = np.asarray([0, 0, 4, 0])
-    real_cov = np.asarray([[1, 0.2, 0, 0.5],
-                           [0.2, 2, 0, 0],
-                           [0, 0, 1, 0],
-                           [0.5, 0, 0, 1]])
+    real_cov = np.asarray(
+        [[1, 0.2, 0, 0.5],
+         [0.2, 2, 0, 0],
+         [0, 0, 1, 0],
+         [0.5, 0, 0, 1]]
+    )
     num_of_samples = 1000
 
-    samples = numpy.random.multivariate_normal(
-        real_mu, real_cov, num_of_samples
+    samples = np.random.multivariate_normal(
+        real_mu, real_cov, size=num_of_samples
     )
 
     multivariate_gaussian = MultivariateGaussian().fit(samples)
-    print(f"Estimated expectation:\n{multivariate_gaussian.mu_}")
-    print(f"Estimated covariance matrix:\n{multivariate_gaussian.cov_}")
+    print(multivariate_gaussian.mu_)
+    print(multivariate_gaussian.cov_)
 
     # Question 5 - Likelihood evaluation
-    X_axis = np.linspace(-10, 10, 200)
-    Y_axis = np.linspace(-10, 10, 200)
+    axis_size = 200
+    max_axis_val = 10
+    min_axis_val = -10
 
-    # t = np.meshgrid(X_axis, Y_axis)
+    f1_axis = np.linspace(min_axis_val, max_axis_val, axis_size)
+    f3_axis = np.linspace(min_axis_val, max_axis_val, axis_size)
 
-    z = []
-
-    for i in X_axis:
-        for j in Y_axis:
-            z.append(MultivariateGaussian.log_likelihood(
-                np.asarray([i, 0, j, 0]), real_cov, samples
+    log_likelihood_values = list()
+    for f1 in f1_axis:
+        for f3 in f3_axis:
+            log_likelihood_values.append(MultivariateGaussian.log_likelihood(
+                np.asarray([f1, 0, f3, 0]), real_cov, samples
             ))
-        print("\r[" + int((i+10)*5)*'*' + (100-int((i+10)*5))*' ' + ']',end='')
 
-    print()
-    zz = np.asarray(z).reshape(200, 200)
-    go.Figure(go.Heatmap(x=X_axis, y=Y_axis, z=zz),
-              layout=go.Layout(title="title", height=300, width=300)).show()
-    # # todo: titles, optimizations, delete printings, use np.meshgrid?
-    # # Question 6 - Maximum likelihood
-    # # todo: derivative?
-    # raise NotImplementedError()
+    log_likelihood_axis = np.asarray(log_likelihood_values).reshape(
+        axis_size, axis_size
+    )
+
+    go.Figure(
+        [go.Heatmap(x=f3_axis, y=f1_axis, z=log_likelihood_axis)],
+        layout=go.Layout(
+            title={"text": TITLE_FIGURE_Q5, "font": TITLE_FONT},
+            xaxis={"title": "f3"},
+            yaxis={"title": "f1"},
+            height=PLOT_HEIGHT, width=PLOT_HEIGHT
+        )).show()
+
+    # Question 6 - Maximum likelihood
+    max_val = np.amax(log_likelihood_axis)
+    argmax_indices = np.where(log_likelihood_axis == max_val)
+    argmax_vals = (
+        convert_index_to_value(argmax_indices[0][0], min_axis_val,
+                               max_axis_val, axis_size),
+        convert_index_to_value(argmax_indices[1][0], min_axis_val,
+                               max_axis_val, axis_size)
+    )
+
+    print(f"max value: {round(max_val, 3)}"
+          f"\nf1,f3: {tuple(round(num, 3) for num in argmax_vals)}")
+
+
+def convert_index_to_value(index: int, min_axis_val, max_axis_val, axis_size
+                           ) -> float:
+    """
+    Converts an index in the axis array into its real value on the heatmap.
+    That is, 0 is converted to min_axis_val, and (axis_size-1) is converted to
+    max_axis_val.
+    """
+    return ((max_axis_val - min_axis_val) * index / (axis_size - 1.0)
+            + min_axis_val)
 
 
 if __name__ == '__main__':
     np.random.seed(0)
-
-    # todo:
-    #  Q1 - write expectation and variance in the pdf
-    #  Q2 - copy graph to pdf
-    #  Q3 - copy graph to pdf, (verify correctness), answer question (max at
-    #       real_mu, get denser as y-value get higher)
     test_univariate_gaussian()
-
-    # todo:
-    #  gaussian_estimators.py - fix and finish MultivariateGaussian
-    #  Q4 - write expectation and cov matrix in the pdf
-    #  Q5 - improve efficiency (using numpy - moodle, WhatsApp),
-    #       meaningful titles, copy graph to pdf, answer question
-    #  Q6 - answer question, check the way to calculate the max
     test_multivariate_gaussian()