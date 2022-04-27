classifiers_evaluation.py: compare_gaussian_classifiers full implementation gaussian_naive_bayes.py: finish implementing predict and likelihood, make variance unbiased fix bug - samples with one feature (wrong access to X.shape[1] - does not exist) (gaussian_naive_bayes.py, linear_discriminant_analysis.py, perceptron.py)

IMLearn/learners/classifiers/gaussian_naive_bayes.py
@@ -48,8 +48,9 @@ class GaussianNaiveBayes(BaseEstimator):
         self.classes_, counts = np.unique(y, return_counts=True)
         self.pi_ = counts / y.shape[0]
 
-        self.mu_ = np.zeros((self.classes_.shape[0], X.shape[1]))
-        self.vars_ = np.zeros((self.classes_.shape[0], X.shape[1]))
+        num_features = X.shape[1] if len(X.shape) > 1 else 1
+        self.mu_ = np.zeros((self.classes_.shape[0], num_features))
+        self.vars_ = np.zeros((self.classes_.shape[0], num_features))
 
         for i, sample in enumerate(X):
             self.mu_[y[i]] += sample
@@ -60,7 +61,7 @@ class GaussianNaiveBayes(BaseEstimator):
             diff = sample - self.mu_[y[i]]
             self.vars_[y[i]] += diff ** 2
 
-        self.vars_ /= counts[:, None]
+        self.vars_ /= counts[:, None] - 1
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -79,15 +80,26 @@ class GaussianNaiveBayes(BaseEstimator):
 
         z = np.ndarray((X.shape[0], self.classes_.shape[0]))
 
+        num_features = X.shape[1] if len(X.shape) > 1 else 1
+
         for i, sample in enumerate(X):
             for k in self.classes_:
-                p_vec = np.array(
-                    [np.log(scipy.stats.norm.pdf(
-                        sample[j], mean=self.mu_[k, j], var=self.vars_[k, j]
-                    )
-                            for j in range(X.shape[1]))]
-                )
-                z[i, k] = np.log(self.pi_[k]) + np.sum(p_vec)
+                l = []
+                for j in range(num_features):
+                    if self.vars_[k, j]:
+                        l.append(
+                            np.log(
+                                scipy.stats.norm.pdf(
+                                    sample[j],
+                                    loc=self.mu_[k, j],
+                                    scale=(self.vars_[k, j]) ** 0.5
+                                )
+                            )
+                        )
+                    else:
+                        l.append(np.log(1 if sample[j] == self.mu_[k, j] else 0))
+
+                z[i, k] = np.log(self.pi_[k]) + np.sum(l)
 
         return np.argmax(z, axis=1)
 
@@ -113,7 +125,7 @@ class GaussianNaiveBayes(BaseEstimator):
         likelihoods = np.ndarray((X.shape[0], self.classes_.shape[0]))
         for k in range(self.classes_.shape[0]):
             n = scipy.stats.multivariate_normal(mean=self.mu_[k],
-                                                cov=self.cov_)
+                                                cov=np.diag(self.vars_[k]))
             for i in range(X.shape[0]):
                 likelihoods[i, k] = n.pdf(X[i]) * self.pi_[k]
 
@@ -138,4 +150,3 @@ class GaussianNaiveBayes(BaseEstimator):
         """
         from ...metrics import misclassification_error
         return misclassification_error(y, self._predict(X))
-

IMLearn/learners/classifiers/linear_discriminant_analysis.py
@@ -53,8 +53,10 @@ class LDA(BaseEstimator):
         self.classes_, counts = np.unique(y, return_counts=True)
         self.pi_ = counts / y.shape[0]
 
-        self.mu_ = np.zeros((self.classes_.shape[0], X.shape[1]))
-        self.cov_ = np.zeros((X.shape[1], X.shape[1]))
+        num_features = X.shape[1] if len(X.shape) > 1 else 1
+
+        self.mu_ = np.zeros((self.classes_.shape[0], num_features))
+        self.cov_ = np.zeros((num_features, num_features))
 
         for i, sample in enumerate(X):
             self.mu_[y[i]] += sample

IMLearn/learners/classifiers/perceptron.py
@@ -77,16 +77,13 @@ class Perceptron(BaseEstimator):
         `self.fit_intercept_`
         """
         n_samples = X.shape[0]
-        n_features = X.shape[1]
+        n_features = X.shape[1] if len(X.shape) > 1 else 1
 
         self.coefs_ = np.zeros(n_features + 1 if self.include_intercept_
                                else n_features)
         fixed_X = (np.c_[X, np.ones(n_samples)]
                    if self.include_intercept_ else X)
 
-        # todo: improve efficiency - multipy y * X outside, and yX @
-        #  self.coefs_ once each iteration. then find the index of a
-        #  non-positive value
         for t in range(self.max_iter_):
             for sample, response in zip(fixed_X, y):
                 if response * (sample @ self.coefs_) <= 0:

exercises/classifiers_evaluation.py
@@ -42,7 +42,8 @@ def run_perceptron():
     values (y-axis) as a function of the training iterations (x-axis).
     """
     for n, f in [("Linearly Separable", "../datasets/linearly_separable.npy"),
-                 ("Linearly Inseparable", "../datasets/linearly_inseparable.npy")]:
+                 ("Linearly Inseparable",
+                  "../datasets/linearly_inseparable.npy")]:
         # Load dataset
         X, y = load_dataset(f)
 
@@ -56,11 +57,12 @@ def run_perceptron():
         p.fit(X, y)
 
         # Plot figure of loss as function of fitting iteration
+        p_title = f"Progression of the Perceptron algorithm over {n} datasets"
         px.line(pd.DataFrame(list(zip(range(1, len(losses)), losses)),
                              columns=['number of iteration', 'loss value']),
                 x="number of iteration",
                 y="loss value",
-                title=n).show()
+                title=p_title).show()
 
 
 def get_ellipse(mu: np.ndarray, cov: np.ndarray):
@@ -80,39 +82,98 @@ def get_ellipse(mu: np.ndarray, cov: np.ndarray):
         scatter: A plotly trace object of the ellipse
     """
     l1, l2 = tuple(np.linalg.eigvalsh(cov)[::-1])
-    theta = atan2(l1 - cov[0, 0], cov[0, 1]) if cov[0, 1] != 0 else (np.pi / 2 if cov[0, 0] < cov[1, 1] else 0)
+    theta = atan2(l1 - cov[0, 0], cov[0, 1]) if cov[0, 1] != 0 else (
+        np.pi / 2 if cov[0, 0] < cov[1, 1] else 0)
     t = np.linspace(0, 2 * pi, 100)
     xs = (l1 * np.cos(theta) * np.cos(t)) - (l2 * np.sin(theta) * np.sin(t))
     ys = (l1 * np.sin(theta) * np.cos(t)) + (l2 * np.cos(theta) * np.sin(t))
 
-    return go.Scatter(x=mu[0] + xs, y=mu[1] + ys, mode="lines", marker_color="black")
+    return go.Scatter(x=mu[0] + xs, y=mu[1] + ys, mode="lines",
+                      marker_color="black", showlegend=False)
 
 
 def compare_gaussian_classifiers():
     """
     Fit both Gaussian Naive Bayes and LDA classifiers on both gaussians1 and gaussians2 datasets
     """
-    for f in ["gaussian1.npy", "gaussian2.npy"]:
+    for n, f in [("gaussian1", "../datasets/gaussian1.npy"),
+                 ("gaussian2", "../datasets/gaussian2.npy")]:
         # Load dataset
-        raise NotImplementedError()
+        X, y = load_dataset(f)
 
         # Fit models and predict over training set
-        raise NotImplementedError()
+        lda = LDA()
+        lda.fit(X, y)
+        y_pred_lda = lda.predict(X)
 
-        # Plot a figure with two suplots, showing the Gaussian Naive Bayes predictions on the left and LDA predictions
-        # on the right. Plot title should specify dataset used and subplot titles should specify algorithm and accuracy
+        gnb = GaussianNaiveBayes()
+        gnb.fit(X, y)
+        y_pred_gnb = gnb.predict(X)
+
+        # Plot a figure with two subplots, showing the Gaussian Naive Bayes
+        # predictions on the left and LDA predictions on the right. Plot
+        # title should specify dataset used and subplot titles should specify
+        # algorithm and accuracy
         # Create subplots
         from IMLearn.metrics import accuracy
-        raise NotImplementedError()
+        GNBacc = accuracy(y, y_pred_gnb)
+        LDAacc = accuracy(y, y_pred_lda)
+        GNB_plot_title = (
+            "Gaussian Naive Bayes classifier. Accuracy: {}"
+                .format(GNBacc)
+        )
+        LDA_plot_title = (
+            "Linear Discriminant Analysis (LDA) classifier. Accuracy: {}"
+                .format(LDAacc)
+        )
+
+        fig = make_subplots(
+            rows=1, cols=2, subplot_titles=(GNB_plot_title, LDA_plot_title)
+        )
 
         # Add traces for data-points setting symbols and colors
-        raise NotImplementedError()
+        fig.add_trace(
+            go.Scatter(x=X[:, 0], y=X[:, 1], mode="markers", showlegend=False,
+                       marker=dict(
+                           color=y_pred_gnb, symbol=class_symbols[y],
+                           colorscale=class_colors(3))),
+            row=1, col=1
+        )
+
+        fig.add_trace(
+            go.Scatter(x=X[:, 0], y=X[:, 1], mode="markers", showlegend=False,
+                       marker=dict(color=y_pred_lda, symbol=class_symbols[y],
+                                   colorscale=class_colors(3))),
+            row=1, col=2
+        )
 
         # Add `X` dots specifying fitted Gaussians' means
-        raise NotImplementedError()
+        fig.add_trace(
+            go.Scatter(x=gnb.mu_[:, 0], y=gnb.mu_[:, 1], mode="markers",
+                       showlegend=False, marker=dict(color="black",
+                                                     symbol="x")),
+            row=1, col=1
+        )
+        fig.add_trace(
+            go.Scatter(x=lda.mu_[:, 0], y=lda.mu_[:, 1], mode="markers",
+                       showlegend=False, marker=dict(color="black",
+                                                     symbol="x")),
+            row=1, col=2
+        )
 
         # Add ellipses depicting the covariances of the fitted Gaussians
-        raise NotImplementedError()
+        for i in range(gnb.classes_.shape[0]):
+            fig.add_trace(
+                get_ellipse(gnb.mu_[i], np.diag(gnb.vars_[i])),
+                row=1, col=1
+            )
+
+            fig.add_trace(
+                get_ellipse(lda.mu_[i], lda.cov_),
+                row=1, col=2
+            )
+
+        fig.show()
 
 
 if __name__ == '__main__':